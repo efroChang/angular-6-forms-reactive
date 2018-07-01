8475ab8b1e9971757b3ab3a6d360d44894ea3517
Eric Chang <efrochang@gmail.com>
2018-07-01 15:48:22 -0500

    Custom Validator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,12 +9,14 @@ import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
 export class AppComponent implements OnInit {
   genders = ['male', 'female'];
 
-  signupForm: FormGroup;            // [KEY]: For Reactive Forms
+  signupForm: FormGroup;              // [KEY]: For Reactive Forms
+
+  forbiddenNames = ['Emma', 'Roni'];  // for custom validation
 
   ngOnInit() {
     this.signupForm = new FormGroup({
       'userData': new FormGroup({
-        'username': new FormControl(null, Validators.required),
+        'username': new FormControl(null, [Validators.required, this.hasForbiddenName.bind(this)]), // [KEY] Use bind(this) for Angular
         'email': new FormControl(null, [Validators.required, Validators.email])
       }),
       'gender': new FormControl('male'),
@@ -29,5 +31,16 @@ export class AppComponent implements OnInit {
   onAddHobby() {
     const control = new FormControl(null, Validators.required);
     (<FormArray>this.signupForm.get('hobbies')).push(control);        // [KEY]: Must cast <FormArray> to be able to push.
+  }
+
+  // Custom Validation
+  hasForbiddenName( control: FormControl ): { [s: string]: boolean } {
+
+    if( this.forbiddenNames.indexOf(control.value) !== -1 )           // Check of the FormControl has a value in forbiddenName array
+    {
+      return { 'nameIsForbidden': true };
+    }
+
+    return null;                                                      // [KEY]: When valid, must return NULL
   }
 }
